ec52cfbe79b7515d56f4328861f2cec60cf9ff04
Meadow <meadowingc@gmail.com>
2024-04-01 10:11:05 -0600

    use chi router and finally format html templates!

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"codeberg.org/meadowingc/mire/constants"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -15,36 +17,49 @@ func main() {
 	}
 
 	s := New()
+	router := chi.NewRouter()
 
-	http.HandleFunc("GET /{$}", s.indexHandler)
-	http.HandleFunc("GET /about", s.aboutHandler)
-	http.HandleFunc("GET /u/{username}/{$}", s.userHandler)
-	http.HandleFunc("GET /u/{username}/blogroll", s.userBlogrollHandler)
-	http.HandleFunc("GET /static/{file}", s.staticHandler)
-	http.HandleFunc("GET /global", s.globalHandler)
-	http.HandleFunc("GET /random", s.visitRandomPostHandler)
-	http.HandleFunc("GET /settings", s.settingsHandler)
-	http.HandleFunc("POST /settings/submit", s.settingsSubmitHandler)
-	http.HandleFunc("GET /login", s.loginHandler)
-	http.HandleFunc("POST /login", s.loginHandler)
-	http.HandleFunc("GET /logout", s.logoutHandler)
-	http.HandleFunc("POST /logout", s.logoutHandler)
-	http.HandleFunc("POST /register", s.registerHandler)
-	http.HandleFunc("GET /feeds/{url}", s.feedDetailsHandler)
+	if constants.DEBUG_MODE {
+		router.Use(middleware.Logger)
+	}
+
+	router.Use(middleware.Heartbeat("/ping"))
+	router.Use(middleware.SetHeader("X-Clacks-Overhead", "GNU Terry Pratchett"))
+
+	// router.Use(middleware.Compress())
+	router.Use(middleware.NoCache)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.CleanPath)
+
+	router.Get("/", s.indexHandler)
+	router.Get("/about", s.aboutHandler)
+	router.Get("/u/{username}", s.userHandler)
+	router.Get("/u/{username}/blogroll", s.userBlogrollHandler)
+	router.Get("/static/{file}", s.staticHandler)
+	router.Get("/global", s.globalHandler)
+	router.Get("/random", s.visitRandomPostHandler)
+	router.Get("/settings", s.settingsHandler)
+	router.Post("/settings/submit", s.settingsSubmitHandler)
+	router.Get("/login", s.loginHandler)
+	router.Post("/login", s.loginHandler)
+	router.Get("/logout", s.logoutHandler)
+	router.Post("/logout", s.logoutHandler)
+	router.Post("/register", s.registerHandler)
+	router.Get("/feeds/{url}", s.feedDetailsHandler)
 
 	// api functions
-	http.HandleFunc("POST /api/v1/set-post-status/{postUrl}", s.apiSetPostReadStatus)
-	http.HandleFunc("GET /api/v1/ping", func(w http.ResponseWriter, r *http.Request) {
+	router.Post("/api/v1/set-post-status/{postUrl}", s.apiSetPostReadStatus)
+	router.Get("/api/v1/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("pong"))
 	})
 
 	// left in-place for backwards compat
-	http.HandleFunc("GET /feeds", s.settingsHandler)
-	http.HandleFunc("POST /feeds/submit", s.settingsSubmitHandler)
+	router.Get("/feeds", s.settingsHandler)
+	router.Post("/feeds/submit", s.settingsSubmitHandler)
 
 	go statsCalculatorProcess(s)
 
 	log.Println("main: listening on http://localhost:5544")
-	log.Fatal(http.ListenAndServe(":5544", nil))
+	log.Fatal(http.ListenAndServe(":5544", router))
 }
