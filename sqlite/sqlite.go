4adaba84d56473f0fc727d842038b3081b9ad273
Meadow <meadowingc@gmail.com>
2024-04-26 10:45:21 -0600

    create database functions to interact with user preferences

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"reflect"
+	"strconv"
 	"strings"
 	"time"
 
@@ -702,4 +704,165 @@ func (db *DB) GetGlobalNumUsers() int {
 		log.Fatal(err)
 	}
 	return count
+}
+
+func (db *DB) GetSingleUserPreference(userId int, preferenceName string) *string {
+	var preferenceValue string
+
+	query := `SELECT preference_value FROM user_preferences WHERE user_id = ? AND preference_name = ?`
+	err := db.sql.QueryRow(query, userId, preferenceName).Scan(&preferenceValue)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Preference not found for this user
+			return nil
+		}
+		log.Fatal("getGenericUserPreference:: QueryRow failed: ", err)
+	}
+
+	return &preferenceValue
+}
+
+func (db *DB) SaveSingleUserPreference(userId int, preferenceName, preferenceValue string) error {
+	// Check if the preference already exists
+	var exists bool
+	err := db.sql.QueryRow("SELECT EXISTS(SELECT 1 FROM user_preferences WHERE user_id = ? AND preference_name = ?)", userId, preferenceName).Scan(&exists)
+	if err != nil {
+		log.Printf("SaveUserPreference:: Error checking if preference exists: %v", err)
+		return err
+	}
+
+	if exists {
+		// Update existing preference
+		lock()
+		_, err := db.sql.Exec("UPDATE user_preferences SET preference_value = ? WHERE user_id = ? AND preference_name = ?", preferenceValue, userId, preferenceName)
+		unlock()
+		if err != nil {
+			log.Printf("SaveUserPreference:: Error updating user preference: %v", err)
+			return err
+		}
+	} else {
+		// Insert new preference
+		lock()
+		_, err := db.sql.Exec("INSERT INTO user_preferences (user_id, preference_name, preference_value) VALUES (?, ?, ?)", userId, preferenceName, preferenceValue)
+		unlock()
+		if err != nil {
+			log.Printf("SaveUserPreference:: Error inserting user preference: %v", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+type UserPreferences struct {
+	NumPostsToShowInHomeScreen       int `db:"numPostsToShowInHomeScreen" default:"300"`
+	NumUnreadPostsToShowInHomeScreen int `db:"numUnreadPostsToShowInHomeScreen" default:"7"`
+}
+
+func SetFieldValue(field reflect.Value, value string) {
+	switch field.Kind() {
+	case reflect.Int:
+		intVal, err := strconv.Atoi(value)
+		if err != nil {
+			log.Fatalf("SetFieldValue:: Error converting preference value to int: %v", err)
+		}
+		field.SetInt(int64(intVal))
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Bool:
+		boolVal, err := strconv.ParseBool(value)
+		if err != nil {
+			log.Fatalf("SetFieldValue:: Error converting preference value to bool: %v", err)
+		}
+		field.SetBool(boolVal)
+	default:
+		log.Fatalf("SetFieldValue:: Unsupported field type: %v", field.Kind())
+	}
+}
+
+func GetDefaultUserPreferences() *UserPreferences {
+	userPreferences := UserPreferences{}
+	valPointer := reflect.ValueOf(&userPreferences)
+	val := valPointer.Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := typ.Field(i)
+		tag := field.Tag.Get("db")
+		if tag == "" {
+			log.Fatalf("GetUserPreferences:: Field %s does not have a 'db' tag", field.Name)
+		}
+
+		defaultValue := field.Tag.Get("default")
+
+		// set the field value taking into account it's type. Also set the
+		// default value if the preference is not found
+		SetFieldValue(val.Field(i), defaultValue)
+	}
+
+	return &userPreferences
+}
+
+func (db *DB) GetUserPreferences(userId int) *UserPreferences {
+	userPreferences := UserPreferences{}
+	valPointer := reflect.ValueOf(&userPreferences)
+	val := valPointer.Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := typ.Field(i)
+		tag := field.Tag.Get("db")
+		if tag == "" {
+			log.Fatalf("GetUserPreferences:: Field %s does not have a 'db' tag", field.Name)
+		}
+
+		preferenceValue := db.GetSingleUserPreference(userId, tag)
+		if preferenceValue == nil {
+			// Preference not found for this user
+			// Set default value
+			defaultValue := field.Tag.Get("default")
+			if defaultValue == "" {
+				log.Fatalf("GetUserPreferences:: Field %s does not have a 'default' tag", field.Name)
+			}
+			preferenceValue = &defaultValue
+		}
+
+		// set the field value taking into account it's type. Also set the
+		// default value if the preference is not found
+		SetFieldValue(val.Field(i), *preferenceValue)
+	}
+
+	return &userPreferences
+}
+
+func (db *DB) SaveUserPreferences(userID int, userPreferences *UserPreferences) {
+	val := reflect.ValueOf(userPreferences).Elem()
+	typ := val.Type()
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := field.Type()
+		fieldName := typ.Field(i).Name
+		dbTag := typ.Field(i).Tag.Get("db")
+
+		// Convert the field value to a string
+		var fieldValue string
+		switch fieldType.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			fieldValue = strconv.FormatInt(field.Int(), 10)
+		case reflect.Bool:
+			fieldValue = strconv.FormatBool(field.Bool())
+		default:
+			log.Fatalf("SaveUserPreferences:: Unsupported type for field %s", fieldName)
+		}
+
+		err := db.SaveSingleUserPreference(userID, dbTag, fieldValue)
+		if err != nil {
+			log.Fatalf(
+				"SaveUserPreferences:: Error saving user preference %s: %v",
+				fieldName,
+				err,
+			)
+		}
+	}
 }
